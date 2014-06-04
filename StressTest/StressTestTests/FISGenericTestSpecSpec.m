031d98b6797bd669c8a6246bddfc7507b228f242
Chris Gonzales <gonzales.chris.a@gmail.com>
2014-06-04 14:10:42 -0400

    add solution

--- StressTest/StressTestTests/FISGenericTestSpecSpec.m
+++ StressTest/StressTestTests/FISGenericTestSpecSpec.m
@@ -23,86 +23,87 @@ describe(@"FISGenericTestSpec", ^{
     describe(@"our generic tests; let's make them pass! Ignore the back-slashes in the below code, those are there so i can still use obj-c syntax in an NSString", ^{
         describe(@"let's work with the beKindOf matcher!", ^{
             it(@"create an NSString and test that it is kind of class NSString",^{
-                <#code#>
+                expect(@"hamburger").to.beKindOf([NSString class]);
             });
             
             it(@"create an NSArray and test that it is kind of class NSArray",^{
-                <#code#>
+                NSArray *breakfast = @[];
+                expect(breakfast).to.beKindOf([NSArray class]);
             });
         });
         
         describe(@"let's work with the equal matcher!", ^{
             it(@"test that 4+4 is equal to 8",^{
-                <#code#>
+                expect(4+4).to.equal(8);
             });
             
             it(@"create an NSString variable called 'name' with the value Chris. test that your variable is equal to @\"Chris\"",^{
-                <#code#>
+                NSString *name = @"Chris";
+                expect(name).to.equal(@"Chris");
             });
             
             it(@"create an NSArray variable named 'healthyFood', set it equal to an array with the NSStrings 'poptarts' and 'hot pockets'. use a test to verify that your variable is equal to @[@\"poptarts\", @\"hotpockets\"]",^{
-                <#code#>
+                NSArray *healthyFood = @[@"poptarts", @"hot pockets"];
+                expect(healthyFood).to.equal(@[@"poptarts", @"hot pockets"]);
             });
         });
         
         describe(@"let's work with the notEqual matcher!", ^{
             it(@"create an NSString variable called 'name' with the value Chris. test that your variable is not equal to the NSString @\"Joe\"",^{
-                <#code#>
+                NSString *name = @"Chris";
+                expect(name).notTo.equal(@"Joe");
             });
             
             it(@"test that 4+4 is not equal to 18",^{
-                <#code#>
+                expect(4+4).toNot.equal(18);
             });
             
             it(@"create an NSArray variable called 'healthyFood', set it equal to an array with the NSStrings 'poptarts' and 'hot pockets'. use a test to verify that your variable is not equal to @[@\"celery\", @\"spinach\"]",^{
-                <#code#>
+                NSArray *healthyFood = @[@"poptarts", @"hot pockets"];
+                expect(healthyFood).toNot.equal(@[@"celery", @"spinach"]);
             });
             
             it(@"create an NSArray variable called 'healthyFood', set it equal to an array with the NSStrings 'poptarts' and 'hot pockets'. use a test to verify that your variable is not equal to @[@\"hotpockets\", @\"poptarts\"]",^{
-                <#code#>
+                NSArray *healthyFood = @[@"poptarts", @"hot pockets"];
+                expect(healthyFood).toNot.equal(@[@"hot pockets", @"poptarts"]);
             });
         });
         
         describe(@"let's work with the beTruthy matcher! make tests below using beTruthy instead of the equal matcher.", ^{
             it(@"test that 4+4 is equal to 8",^{
-                <#code#>
+                expect(4+4 == 8).to.beTruthy();
             });
             
             it(@"create an NSString variable called 'name' with the value Chris. test that your variable is equal to @\"Chris\"",^{
-                <#code#>
+                NSString *name = @"Chris";
+                expect([name isEqualToString:@"Chris"]).to.beTruthy();
             });
             
-            it(@"create an NSArray variable named 'healthyFood', set it equal to an array with the NSStrings 'poptarts' and 'hot pockets'. use a test to verify that your variable is equal to @[@\"poptarts\", @\"hotpockets\"]",^{
-                <#code#>
-            });
         });
         
         describe(@"let's work with the beFalsy matcher! make tests below using beFalsy instead of the notEqual matcher", ^{
             it(@"create an NSString variable called 'name' with the value Chris. test that your variable is not equal to the NSString @\"Joe\"",^{
-                <#code#>
+                NSString *name = @"Chris";
+                expect([name isEqualToString:@"Joe"]).to.beFalsy();
             });
             
             it(@"test that 4+4 is not equal to 18",^{
-                <#code#>
+                expect(4+4 == 18).to.beFalsy();
             });
             
-            it(@"create an NSArray variable called 'healthyFood', set it equal to an array with the NSStrings 'poptarts' and 'hot pockets'. use a test to verify that your variable is not equal to @[@\"celery\", @\"spinach\"]",^{
-                <#code#>
-            });
-            
-            it(@"create an NSArray variable called 'healthyFood', set it equal to an array with the NSStrings 'poptarts' and 'hot pockets'. use a test to verify that your variable is not equal to @[@\"hotpockets\", @\"poptarts\"]",^{
-                <#code#>
-            });           
         });
         
         describe(@"let's finish with some collection-specific matchers, beginsWith and endsWith", ^{
             it(@"re-create your 'healthFood' array from earlier in the exercise. test that 'healthFood' begins with @\"poptarts\"",^{
-                <#code#>
+                NSArray *healthyFood = @[@"poptarts", @"hot pockets"];
+                expect(healthyFood).to.beginWith(@[@"poptarts"]);
             });
             
-            it(@"re-create your 'healthFood' array from earlier in the exercise. test that 'healthFood' ends with with @\"hotpockets\"",^{
-                <#code#>
+            it(@"re-create your 'healthFood' array from earlier in the exercise. test that 'healthFood' ends with with @\"hot pockets\"",^{
+                NSArray *healthyFood = @[@"poptarts", @"hot pockets"];
+                expect(healthyFood).to.endWith(@[@"hot pockets"]);
             });
+            
         });
         
     });
